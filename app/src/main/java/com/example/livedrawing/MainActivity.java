c9791981f298817a52a0e4ece7e9bdf63e6f67b4
haschish <haschish@gmail.com>
2020-04-08 14:20:32 +0500

    coding MainActivity

--- app/src/main/java/com/example/livedrawing/MainActivity.java
+++ app/src/main/java/com/example/livedrawing/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.livedrawing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 
 public class MainActivity extends AppCompatActivity {
+    private LiveDrawingView liveDrawingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        liveDrawingView = new LiveDrawingView(this, size.x, size.y);
+
+        setContentView(liveDrawingView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
