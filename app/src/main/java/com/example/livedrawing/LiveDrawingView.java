55bf0e4de26b058a74ef85019e0e1e2d866cbd58
haschish <haschish@gmail.com>
2020-04-08 16:32:15 +0500

    added instance variables

--- app/src/main/java/com/example/livedrawing/LiveDrawingView.java
+++ app/src/main/java/com/example/livedrawing/LiveDrawingView.java
@@ -1,9 +1,25 @@
 package com.example.livedrawing;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class LiveDrawingView extends SurfaceView {
+    private final boolean DEBUGGING = true;
+
+    private SurfaceHolder holder;
+    private Canvas canvas;
+    private Paint paint;
+
+    private long fps;
+    private final int MS_IN_SECONDS = 1000;
+
+    private int screenX;
+    private int screenY;
+    private int fontSize;
+    private int fontMargin;
 
     public LiveDrawingView(Context context, int x, int y) {
         super(context);
