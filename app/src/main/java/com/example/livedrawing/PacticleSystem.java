9c8d320a6ed322b56e7b1ff691fc2f8c905dfee1
haschish <haschish@gmail.com>
2020-04-10 14:35:58 +0500

    start coding ParticleSystem

--- app/src/main/java/com/example/livedrawing/PacticleSystem.java
+++ app/src/main/java/com/example/livedrawing/PacticleSystem.java
@@ -1,4 +1,12 @@
 package com.example.livedrawing;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class PacticleSystem {
+    private float duration;
+
+    private ArrayList<Particle> particles;
+    private Random random = new Random();
+    boolean isRunning = false;
 }
