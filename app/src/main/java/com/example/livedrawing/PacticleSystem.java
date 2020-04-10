16d97240b05be285db06f71845d3f31b8c4b186b
haschish <haschish@gmail.com>
2020-04-10 14:40:24 +0500

    added method init to ParticleSystem

--- app/src/main/java/com/example/livedrawing/PacticleSystem.java
+++ app/src/main/java/com/example/livedrawing/PacticleSystem.java
@@ -1,5 +1,7 @@
 package com.example.livedrawing;
 
+import android.graphics.PointF;
+
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -9,4 +11,19 @@ public class PacticleSystem {
     private ArrayList<Particle> particles;
     private Random random = new Random();
     boolean isRunning = false;
+
+
+    void init(int numParticles) {
+        particles = new ArrayList<>();
+
+        for (int i = 0; i < numParticles; i++) {
+            float angle = random.nextInt(360);
+            angle = angle * 3.14f / 180;
+
+            float speed = (random.nextInt(10) + 1); // быстрые частицы
+
+            PointF direction = new PointF((float)Math.cos(angle) * speed, (float)Math.sin(angle) * speed);
+            particles.add(new Particle(direction));
+        }
+    }
 }
