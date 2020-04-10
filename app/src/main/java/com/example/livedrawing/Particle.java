5d00d518df92c401729abbc093bc9855f461e3ea
haschish <haschish@gmail.com>
2020-04-10 14:27:41 +0500

    added constructor Particle

--- app/src/main/java/com/example/livedrawing/Particle.java
+++ app/src/main/java/com/example/livedrawing/Particle.java
@@ -1,4 +1,16 @@
 package com.example.livedrawing;
 
+import android.graphics.PointF;
+
 public class Particle {
+    PointF velocity;
+    PointF position;
+
+    Particle(PointF direction) {
+        velocity = new PointF();
+        position = new PointF();
+
+        velocity.x = direction.x;
+        velocity.y = direction.y;
+    }
 }
